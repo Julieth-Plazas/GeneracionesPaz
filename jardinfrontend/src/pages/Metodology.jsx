425da3c2f417d8c0b4cd7d7ebae94d2f80dfa9fd
natalia mogollón <110124286+nathis06@users.noreply.github.com>
2023-08-30 13:17:44 -0500

    Metodology

--- jardinfrontend/src/pages/Metodology.jsx
+++ jardinfrontend/src/pages/Metodology.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Metodology = () => {
   return (
-    <div>Metodology</div>
+    <div className="p-10">
+              <h1 className="text-5xl font-bold">Jornada</h1>
+              <p className="py-6">
+               <li className="pb-2">7:30 - 8.00   Juego Libre </li>
+               <li className="pb-2">8.00 -  8:30 Actividades de Rutina</li>
+               <li className="pb-2">8:30 - 9:30 Actividad pedagógica</li>
+               <li className="pb-2">9:30 – 10:15  onces -Parque</li>
+               <li className="pb-2">10:15 - 11:00 Actividad pedagógica</li>
+               <li className="pb-2">11:00 - 11:45  Actividad pedagógica</li>
+              </p>
+
+              </div>
   )
 }
 
