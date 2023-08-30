2812efac1ef8f5b0ee7f8a23c05c8beb75e3c6b0
Julieth-Plazas <juliethpr9@gmail.com>
2023-08-29 23:21:47 -0500

    Mision

--- jardinfrontend/src/pages/About.jsx
+++ jardinfrontend/src/pages/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+            <div className="p-10">
+              <h1 className="text-5xl font-bold">MISION</h1>
+              <p className="py-6">
+              Ofrecer  atención integral a la primera infancia con calidad, dentro de la normatividad vigente y el 
+              servicio de educación preescolar, desarrollando competencias generales para potenciar a los niños  en 
+              forma integral.
+              </p>
+              
+            </div>
+            
+              )
 }
 
 export default About
