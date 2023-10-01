550e78378d931a1a68c546b7886446c54007b7a4
SebastianL2 <sebas16cely@gmail.com>
2023-09-30 22:07:36 -0500

    hu

--- jardinfrontend/src/pages/TeacherPages/Games.jsx
+++ jardinfrontend/src/pages/TeacherPages/Games.jsx
@@ -5,55 +5,52 @@ import Game3 from '../Games/MemoriaVocal'
 import Game4 from '../Games/GamesSumRes1'
 const Games = () => {
   return (
-    <div>
-    <div className="collapse collapse-plus w-15 collapse-arrow bg-base-200">
+    <div className=' bg-white'>
+     <div className="collapse collapse-plus w-15 collapse-arrow bg-white">
     <input type="radio" name="my-accordion-2" checked="checked" /> 
     <div className="collapse-title text-xl font-medium">
       Juego 1
     </div>
-    <div className="collapse-content " > 
+    <div className="collapse-content"> 
       <Game1></Game1>
     </div>
   </div>
-  <div className="collapse collapse-arrow bg-base-200">
+  <div className="collapse collapse-arrow bg-white">
     <input type="radio" name="my-accordion-2" /> 
     <div className="collapse-title text-xl font-medium">
-    Juego 2
+      Juego 2
     </div>
     <div className="collapse-content"> 
-    <Game2></Game2>
+      <Game2></Game2>
     </div>
   </div>
-  <div className="collapse collapse-arrow bg-base-200">
+  <div className="collapse collapse-arrow bg-white">
     <input type="radio" name="my-accordion-2" /> 
     <div className="collapse-title text-xl font-medium">
-    Juego 3
+      Juego 3
     </div>
     <div className="collapse-content"> 
-    <Game3></Game3>
+      <Game3></Game3>
     </div>
   </div>
-
-  <div className="collapse collapse-arrow bg-base-200">
+  <div className="collapse collapse-arrow bg-white">
     <input type="radio" name="my-accordion-2" /> 
     <div className="collapse-title text-xl font-medium">
-    Juego 4
+      Juego 4
     </div>
     <div className="collapse-content"> 
-    <Game4></Game4>
+      <Game4></Game4>
     </div>
   </div>
-  <div className="collapse collapse-arrow bg-base-200">
+  <div className="collapse collapse-arrow bg-white">
     <input type="radio" name="my-accordion-2" /> 
     <div className="collapse-title text-xl font-medium">
-    Juego 5
+      Juego 5
     </div>
     <div className="collapse-content"> 
-    <iframe src="https://v6p9d9t4.ssl.hwcdn.net/html/5137922/index.html" title="sd" width="100%" height="500"></iframe>
+      <iframe src="https://v6p9d9t4.ssl.hwcdn.net/html/5137922/index.html" title="sd" width="100%" height="500"></iframe>
     </div>
   </div>
-
-
   </div>
   )
 }
